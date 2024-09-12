c2cc24c69accdb547cdfe3924c8adb9f60234de3
dilya <mukhambetovad49@mail.ru>
2024-09-13 00:33:18 +0500

    add assigment 1

---
+++ assigment 1/1.swift
@@ -0,0 +1,23 @@
+// step 1
+let firstName = "Dilyara"
+let lastName = "Mukhambetova"
+let age = 20
+let birthYear = 2004
+let isStudent = true
+let height = 1.79
+
+// step 2
+let hobby = "gym"
+let numberOfHobbies = 7
+let favoriteNumber = 1
+let isHobbyCreave = false
+
+// step 3
+let lifeStory = """
+My name is \(firstName) \(lastName). I am \(age) years old, born in \(birthYear). I am currently a student.
+ I enjoy \(hobby), which is a creative hobby.
+ I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber).
+"""
+
+// step 4
+print(lifeStory)
